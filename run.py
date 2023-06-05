f9be12cf710d213c9a88c1fc77f0fc191bc5e6e0
alanpaterson82 <>
2023-06-05 07:34:42 +0000

    refactor update worksheet funtions into one

--- run.py
+++ run.py
@@ -55,26 +55,26 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-
-    """
-    Update surplus worksheet, add new row with the list data provided.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
+# def update_sales_worksheet(data):
+#
+#    """
+#    Update sales worksheet, add new row with the list data provided.
+#    """
+#    print("Updating sales worksheet...\n")
+#    sales_worksheet = SHEET.worksheet("sales")
+#    sales_worksheet.append_row(data)
+#    print("Sales worksheet updated successfully.\n")
+#
+#
+# def update_surplus_worksheet(data):
+#
+#    """
+#    Update surplus worksheet, add new row with the list data provided.
+#    """
+#    print("Updating surplus worksheet...\n")
+#    surplus_worksheet = SHEET.worksheet("surplus")
+#    surplus_worksheet.append_row(data)
+#    print("Surplus worksheet updated successfully.\n")
 
 
 def update_worksheet(data, worksheet):
